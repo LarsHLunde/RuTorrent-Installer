8ca28d8117259bc3222beef476539eb0fe4523ea
Lars Hudson Lunde <laffen47@yahoo.no>
2017-05-05 15:59:34 +0200

    added more features and removed messages file

--- installer.lua
+++ installer.lua
@@ -6,20 +6,23 @@ function file2table(file,atable)
 	fp:close()
 end
 
-local messages = {}
 local commands = {}
 
-file2table("./resources/messages.txt", messages)
 file2table("./resources/commands.txt", commands)
 
-print(messages[1])
-print(messages[2])
-print(messages[3])
+print("Welcome to the Raspberry PI RuTorrent Installer")
+print("created by Pyro_Killer")
+print("It is currently in early pre-dev and will not work at all")
 
 for i = 1, #commands do
 	local x, y = commands[i]:find(";")
 	print(commands[i]:sub(x+1))
 	os.execute(commands[i]:sub(1,x-1))
-
 end
 
+
+os.execute("sudo lua ./resources/rewriter.lua ")
+print("Restarting apache")
+os.execute(sudo service apache2 restart >> /dev/null)
+print("Starting rTorrent")
+os.execute(screen -S rtorrent -fa -d -m rtorrent)
--- resources/rewriter.lua
+++ resources/rewriter.lua
@@ -1,3 +1,7 @@
+-- The first argument is where the rTorrent downloading
+---files should be placed. The second argument is the home 
+-- firectory of
+
 function replaceVars(file,keywords,replacees,replacers)
 	local edit_file = io.open(file, "r")
 	local text = {}
@@ -23,17 +27,32 @@ function replaceVars(file,keywords,replacees,replacers)
 
 end
 
-local config_file = "./config.php"
+print("Rewriting rutorrent config file")
+local config_file = "/var/www/html/rutorrent"
 local config_keywords = {"\"php\" 	=>","\"curl\"	=>","\"gzip\"	=>","\"id\"	=>","\"stat\"	=>"}
 local config_replacees = {"\'\'","\'\'","\'\'","\'\'","\'\'"}
 local config_replacers = {"'/usr/bin/php'","'/usr/bin/curl'","'/bin/gzip'","'/usr/bin/id'","'/usr/bin/stat'"}
+--replaceVars(config_file,config_keywords,config_replacees,config_replacers)
 
 
+print("Adding rtorrent to startup")
+local startup_fix_file = "/etc/rc.local"
+local startup_fix_keywords = {"su -c "screen -S rtorrent -fa -d -m rtorrent" pi "}
+local startup_fix_replacees = {"su -c "screen -S rtorrent -fa -d -m rtorrent" pi "}
+local startup_fix_replacers = {""}
+--replaceVars(startup_fix_file,startup_fix_keywords,startup_fix_replacees,startup_fix_replacers)
 local startup_file = "/etc/rc.local"
 local startup_keywords = {"exit 0"}
 local startup_replacees = {"exit 0"}
-local startup_replacers = {"screen -S rtorrent -fa -d -m rtorrent \nexit 0"}
-
-
---replaceVars(config_file,config_keywords,config_replacees,config_replacers)
+local startup_replacers = {"su -c "screen -S rtorrent -fa -d -m rtorrent" pi \nexit 0"}
 --replaceVars(startup_file,startup_keywords,startup_replacees,startup_replacers)
+
+
+print("Rewriting the rTorrent config file")
+local torrentrc_file = arg[2] .. "/.rtorrent.rc"
+local torrentrc_keywords = {"KEYWORD"}
+local torrentrc_replacees = {"KEYWORD"}
+local torrentrc_replacers = {"arg[1]"}
+--replaceVars(torrentrc_file,torrentrc_keywords,torrentrc_replacees,torrentrc_replacers)
+
+
