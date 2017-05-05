8a8f7be584e6e5e418830706b416d92ac48d7ef7
Lars Hudson Lunde <laffen47@yahoo.no>
2017-05-05 13:29:24 +0200

    Preparing for first test

--- installer.lua
+++ installer.lua
@@ -23,9 +23,34 @@ function replaceVars(file,keywords,replacees,replacers)
 
 end
 
+function file2table(file,atable)
+	local fp = io.open(file, "r")
+	for line in fp:lines() do
+		table.insert (atable, line)
+	end
+	fp:close()
+end
+
+local messages = {}
+local commands = {}
+
+file2table("./resources/messages.txt", messages)
+file2table("./resources/commands.txt", commands)
+
+print(messages[1])
+print(messages[2])
+print(messages[3])
+
+for i = 1, #commands do
+	local x, y = commands[i]:find(";")
+	print(commands[i]:sub(x+1))
+	print(commands[i]:sub(1,x-1))
+
+end
+
 local config_file = "./config.php"
 local config_keywords = {"\"php\" 	=>","\"curl\"	=>","\"gzip\"	=>","\"id\"	=>","\"stat\"	=>"}
 local config_replacees = {"\'\'","\'\'","\'\'","\'\'","\'\'"}
 local config_replacers = {"'/usr/bin/php'","'/usr/bin/curl'","'/bin/gzip'","'/usr/bin/id'","'/usr/bin/stat'"}
 
-replaceVars(config_file,config_keywords,config_replacees,config_replacers)
+--replaceVars(config_file,config_keywords,config_replacees,config_replacers)
