584c46b9d64971c06d110a6f080a21e490de48ec
Lars Hudson Lunde <laffen47@yahoo.no>
2017-08-13 22:19:25 +0200

    fixed no tty bug

--- helper.lua
+++ helper.lua
@@ -84,6 +84,6 @@ if commandOutput("whoami") == "root\n" or true then
 	
 	print("The script will now start the main script installation script")
 	continue()
-	os.execute("su - " .. newUser .. " -c \"git clone https://github.com/LarsHLunde/RuTorrent-Installer.git && cd RuTorrent-Installer && lua installer.lua\"")
+	os.execute("su - " .. newUser .. " -s /bin/bash -c \"cd ~ && git clone https://github.com/LarsHLunde/RuTorrent-Installer.git && cd RuTorrent-Installer && lua installer.lua\"")
 end
 
