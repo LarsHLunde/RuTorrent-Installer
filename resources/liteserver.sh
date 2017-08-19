5ffc75032916d7c171f895ee29543244ddc3c8df
Lars Hudson Lunde <laffen47@yahoo.no>
2017-08-19 22:30:35 +0200

    updated to 1.3

--- resources/liteserver.sh
+++ resources/liteserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~
-git clone -b test https://github.com/LarsHLunde/RuTorrent-Installer.git
+git clone https://github.com/LarsHLunde/RuTorrent-Installer.git
 cd RuTorrent-Installer/
 lua installer.lua
